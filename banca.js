ea74d77e0f515c9e85657294a0fafca510a39666
kavidu <kaviduwarnakulasuriya@gmail.com>
2023-11-21 16:48:02 +0100

    first commit

--- banca.js
+++ banca.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Account = /** @class */ (function () {
     function Account(_deposit, _prelievo) {
         this.balanceInit = 0; //saldo attuale
@@ -11,3 +26,18 @@ var Account = /** @class */ (function () {
 }());
 var SonAccount = new Account(100, 100);
 console.log(SonAccount);
+var MainAccount = /** @class */ (function (_super) {
+    __extends(MainAccount, _super);
+    function MainAccount(_deposit, _prelievo) {
+        var _this = _super.call(this, _deposit, _prelievo) || this;
+        _this.contoDopoDeposito = -(_deposit / 10) + _deposit;
+        _this.contoDopoPrelievo = -(_prelievo / 10) + _prelievo;
+        return _this;
+    }
+    MainAccount.prototype.verificaConto = function () {
+        return "".concat(this.contoDopoDeposito, " ").concat(this.contoDopoPrelievo);
+    };
+    return MainAccount;
+}(Account));
+var MotherAccount = new MainAccount(100, 100);
+console.log(MotherAccount);
--- banca.ts
+++ banca.ts
@@ -1,26 +1,36 @@
 class Account {
-    
-    balanceInit:number=0; //saldo attuale
+  balanceInit: number = 0; //saldo attuale
 
-    oneDeposit:number;//deposito
-    oneWithDraw:number;//versamento
+  oneDeposit: number; //deposito
+  oneWithDraw: number; //versamento
 
-    constructor(_deposit:number,_prelievo:number){
-        this.oneDeposit = _deposit;// versamento
-        this.oneWithDraw = _prelievo;// prelievo
-    }
+  constructor(_deposit: number, _prelievo: number) {
+    this.oneDeposit = _deposit; // versamento
+    this.oneWithDraw = _prelievo; // prelievo
+  }
 
-    verificaConto(){
-        return `${this.oneDeposit} ${this.oneWithDraw}`
-    }
+  verificaConto() {
+    return `${this.oneDeposit} ${this.oneWithDraw}`;
+  }
 }
 
- let SonAccount = new Account (100,100);
- console.log(SonAccount);
+let SonAccount = new Account(100, 100);
+console.log(SonAccount);
 
- class MotherAccountt extends Account{
-    //oneDeposit = ((this._deposit * 10%) - this._deposit);
-    constructor(_deposit:number,_prelievo:number){
-       super() 
-    }
- }
+class MainAccount extends Account {
+  //oneDeposit = ((this._deposit * 10%) - this._deposit);
+  contoDopoDeposito: number;
+  contoDopoPrelievo: number;
+
+  constructor(_deposit: number, _prelievo: number) {
+    super(_deposit, _prelievo);
+    this.contoDopoDeposito = -(_deposit / 10) + _deposit;
+    this.contoDopoPrelievo = -(_prelievo / 10) + _prelievo;
+  }
+  verificaConto(): string {
+    return `${this.contoDopoDeposito} ${this.contoDopoPrelievo}`;
+  }
+}
+
+let MotherAccount = new MainAccount(100, 100);
+console.log(MotherAccount);
